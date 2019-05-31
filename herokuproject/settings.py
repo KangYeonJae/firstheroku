f1ec8ba23f93b8889088e10ebaa7e0d6b1237281
citrony1998 <yjzzang1998@naver.com>
2019-06-01 00:11:51 +0900

    django-heroku modify settings

--- herokuproject/settings.py
+++ herokuproject/settings.py
@@ -20,11 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cdaz4=jwu!xzr_&m7+l^9rpkv-38y-f&+b-7!(64_=m4x*393c'
+# SECRET_KEY = 'cdaz4=jwu!xzr_&m7+l^9rpkv-38y-f&+b-7!(64_=m4x*393c'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cdaz4=jwu!xzr_&m7+l^9rpkv-38y-f&+b-7!(64_=m4x*393c')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
 ALLOWED_HOSTS = []
 
 
